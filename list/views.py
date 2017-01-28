676dd9908e981bdc30d7a5169a81d4fce3e25d84
sooraj <soorajparakkattil@gmail.com>
2017-01-28 21:50:41 +0530

    templates

--- list/views.py
+++ list/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+#from django.http import http404
+from django.template import loader
 from .models import Product
 
 # Create your views here.
 def index(request):
-    all_products=Product.objects.all()
-    html=''
-    for product in all_products:
-        url=''+str(product.product_id)+'/'
-        html+='<a href="'+ url +'">'+ product.pname +'</a><br>'
-    return HttpResponse(html)
+    all_products = Product.objects.all()
+    context = {'all_products': all_products}
+    return render(request, 'list/index.html', context)
+
 def detail(request,product_id):
-    return HttpResponse("<h1>hello"+str(product_id) +"</ h1>")
+    product=get_object_or_404(Product,product_id=product_id)
+    return render(request, 'list/detail.html', {'product':product})
